8ddc7e0dbc0050c5bfcc3693859d9ecfc6a2ef05
paulastm <paulastm@gmail.com>
2018-01-23 22:45:37 -0200

    atualização das pastas

---
+++ Semana 3/split.py
@@ -0,0 +1,4 @@
+"""funcao split: exemplo"""
+nome,sobrenome=input("Digite seu primeiro e último nome ").split()
+print(nome)
+print(sobrenome)
---
+++ Semana 3/split2.py
@@ -0,0 +1,7 @@
+"""funcao split: exemplo"""
+x,y,z=input("Digite três numeros ").split()
+print(x,y,z)
+
+total=y+5
+
+print(total)
