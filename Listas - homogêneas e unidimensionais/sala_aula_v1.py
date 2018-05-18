7b064b292587611f7c28b908839f41922d2d8fe8
Paulo <pauloafpjunior@hotmail.com>
2018-05-18 14:46:05 -0300

    melhorando codigo lista

--- Listas - homogêneas e unidimensionais/sala_aula_v1.py
+++ Listas - homogêneas e unidimensionais/sala_aula_v1.py
@@ -1,9 +1,10 @@
 nota_aluno1 = float(input())
 nota_aluno2 = float(input())
+nota_aluno3 = float(input())
 
-media_sala = (nota_aluno1 + nota_aluno2) / 2
+media_sala = (nota_aluno1 + nota_aluno2 + nota_aluno3) / 3
 
 print("Média da sala: %.2f" % media_sala)
 print("Aluno 1: %.2f" % (nota_aluno1 - media_sala))
 print("Aluno 2: %.2f" % (nota_aluno2 - media_sala))
-
+print("Aluno 3: %.2f" % (nota_aluno3 - media_sala))
--- Listas - homogêneas e unidimensionais/sala_aula_v2.py
+++ Listas - homogêneas e unidimensionais/sala_aula_v2.py
@@ -1,12 +1,12 @@
-num_alunos = int(input())
-soma_nota = 0.0
-i = 0
-notas = []
+num_alunos = int(input()) # lê a quantidade de alunos da sala
+soma_nota = 0.0 # "soma_nota" é um exemplo de acumulador
+i = 0 # "i" é um exemplo de contador
+notas = [] # "notas" é uma lista vazia
 
-# lê as notas dos alunos e adiciona na lista "notas"
+# lê a nota do aluno e a adiciona na lista "notas"
 while i < num_alunos:
 	nota = float(input())
-	soma_nota += nota # "soma_nota" é um exemplo de acumulador
+	soma_nota += nota 
 	notas.append(nota)
 	i += 1
 
@@ -14,9 +14,8 @@ media_sala = soma_nota / num_alunos
 print("Média da sala: %.2f" % media_sala)
 
 # imprime a diferença de notas dos alunos
-i = 0
+i = 0 # é importante inicializar o contador aqui
 while i < num_alunos:	
 	print("Aluno %d: %.2f" % (i + 1, notas[i] - media_sala))
 	i += 1
-
 
